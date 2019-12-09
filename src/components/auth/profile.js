8d2ddcf2c07ff469676bff7769cb2ca5b351839e
Kirk Hilse <khilsedev@gmail.com>
2019-12-09 10:48:48 -0800

    Minor CSS tweak to profile

--- src/components/auth/profile.js
+++ src/components/auth/profile.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+
+const cardStyle = {
+    display: 'block',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    width: '30vw',
+    height: '40vw',
+    transform: 'translate(-50%,-50%)',
+    transitionDuration: '0.3s',
+    boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)"
+}
 
 const Profile = props => {
     return (
-        <div className="profile-container">
-            <h1>Profile</h1>
-            <img className="profile-image" src={props.user.profileImage} alt="profile" />
-            <p>Full Name: {props.user.fullName}</p>
-            <p>First Name: {props.user.firstName}</p>
-            <p>Last Name: {props.user.lastName}</p>
-            <p>email: {props.user.email}</p>
-        </div>
+        <Card raised="true" style={cardStyle}>
+            <div>
+                <h1>Profile</h1>
+                <img className="profile-image" src={props.user.profileImage} alt="profile" />
+                <p>Full Name: {props.user.fullName}</p>
+                <p>First Name: {props.user.firstName}</p>
+                <p>Last Name: {props.user.lastName}</p>
+                <p>Email: {props.user.email}</p>
+            </div>
+        </Card>
     )
 }
 
--- src/components/pages/astronauts.js
+++ src/components/pages/astronauts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
 import Carousel from 'react-material-ui-carousel';
 
 const Astronauts = props => {
@@ -8,12 +8,12 @@ const Astronauts = props => {
             <h1>There are {props.astronauts.length} People in Space</h1>
             <Carousel animation="slide" interval="3000">
                 {props.astronauts.map((astronaut, i) => {
-                    return <Paper key={i}>
+                    return <Card key={i}>
                         <div className="astronaut-paper">
                             <h1>{astronaut.name}</h1>
                             <h2>Aboard the <i>{astronaut.craft}</i></h2>
                         </div>
-                        </Paper>
+                        </Card>
                 })}
             </Carousel>
         </div>
